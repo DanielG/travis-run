eb72895b839a312e536e813314acdf2571d5593b
Daniel Gröber <dxld@darkboxed.org>
2014-06-27 20:09:24 +0200

    Comment

--- travis-run-create.sh
+++ travis-run-create.sh
@@ -13,6 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# This script will be sourced by travis-run
 ## Create travis build VM on the current machine
 
 if [ ! -e .travis.yml ]; then
