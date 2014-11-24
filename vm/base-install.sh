f0b2e0176e448978d32dd501ab9e6d6e10be91d4
Daniel Gröber <dxld@darkboxed.org>
2014-11-24 21:59:25 +0100

    Fix chef-solo again

--- vm/base-install.sh
+++ vm/base-install.sh
@@ -10,8 +10,6 @@ apt-get install \
 
 gem install --no-ri --no-rdoc bundler
 bundler install --binstubs
-ln -s "$PWD/bin/chef-solo" /usr/local/bin/chef-solo || true
-ls -l /usr/local/bin
 
 wget -O - https://github.com/travis-ci/travis-cookbooks/archive/master.tar.gz | tar -xz
 mkdir -p /var/chef/cookbooks
